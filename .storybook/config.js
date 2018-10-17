e2daebe8560337bb47aab628e16325db1235f321
大沼 峻徳 <ohnhistory@gmail.com>
2018-10-17 15:29:16 +0900

    storybookの場所を移動

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,9 +3,9 @@ import { configure } from '@storybook/react';
 import '~root/src/css/style.scss';
 
 function loadStories() {
-  require('../stories/TodoInputter.js');
-  require('../stories/Todo.js');
-  require('../stories/TodoList.js');
+  require('./stories/TodoInputter.js');
+  require('./stories/Todo.js');
+  require('./stories/TodoList.js');
 }
 
 configure(loadStories, module);
